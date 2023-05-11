f4f3fe141af73f21402cd912aec0b6aa0f059c46
Gustavohp1708 <gustavo.dev17@outlook.com>
2023-05-10 22:05:50 -0300

    insert DozerMapper / Value Object(VO)

--- src/main/java/br/com/rest/repositories/PersonRepository.java
+++ src/main/java/br/com/rest/repositories/PersonRepository.java
@@ -2,6 +2,6 @@ package br.com.rest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.rest.model.Person;
+import br.com.rest.model.PersonVO;
 
-public interface PersonRepository extends JpaRepository<Person, Long>{}
+public interface PersonRepository extends JpaRepository<PersonVO, Long>{}
